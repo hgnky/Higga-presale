fbb8ba8d519f1bf4eebda8f13f97a59f1e716609
Krypto-dev <86320602+krypto-dev@users.noreply.github.com>
2022-03-25 09:54:32 +1000

    Update Presale.jsx

--- src/views/Presale/Presale.jsx
+++ src/views/Presale/Presale.jsx
@@ -99,7 +99,7 @@ function Presale() {
       return dispatch(error("You cannot deposit more than your BNB balance."));
     } else if (action === "presale" && gweiValue.gt(ethers.utils.parseUnits(maxEthlimit, "ether"))) {
       return dispatch(error("You cannot deposit more than Max contribute amount."));
-    } else if (action === "presale" && gweiValue.st(ethers.utils.parseUnits(minEthlimit, "ether"))) {
+    } else if (action === "presale" && gweiValue.lt(ethers.utils.parseUnits(minEthlimit, "ether"))) {
       return dispatch(error("You cannot deposit more than Max contribute amount."));
     }
     await dispatch(changeDeposit({ address, action, value: quantity.toString(), provider, networkID: chainID }));
